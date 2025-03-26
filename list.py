d137d0b73e37c9abbe9de4bdef45f276052238bf
oluwoleowoeye <wolexcharles@yahoo.com>
2025-03-26 18:58:39 +0100

    my second python assignment on PLP academy

---
+++ list.py
@@ -0,0 +1,12 @@
+wole_list=[]
+wole_list.append(10)
+wole_list.append(20)
+wole_list.append(30)
+wole_list.append(40)
+wole_list.insert(1,15)
+wole_list.extend([50,60,70])
+wole_list.pop()
+wole_list.sort()
+index_of_30=wole_list.index(30)
+print(wole_list)
+print("Index of 30:",index_of_30)
